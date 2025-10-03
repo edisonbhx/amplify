0d9ee5096af4a9b904bf53ea0f6de25d91cd9953
edisonbhx <113216948+edisonbhx@users.noreply.github.com>
2025-10-03 08:33:22 -0500

    Add files via upload

---
+++ libs/mockProductos.ts
@@ -0,0 +1,7 @@
+export const getProductos = async () => {
+  return [
+    { nombre: "Cuenta Ahorros", tipo: "Ahorros", numero: "123456" },
+    { nombre: "Cuenta Corriente", tipo: "Corriente", numero: "654321" },
+    { nombre: "Cuenta Nómina", tipo: "Nómina", numero: "112233" },
+  ];
+};
---
+++ libs/mockSaldos.ts
@@ -0,0 +1,7 @@
+export const getSaldos = async () => {
+  return [
+    { numero: "123456", saldo: 150000 },
+    { numero: "654321", saldo: 320000 },
+    { numero: "112233", saldo: 50000 },
+  ];
+};
